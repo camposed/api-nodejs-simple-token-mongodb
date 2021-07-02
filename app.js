0b125f121f0534da39521f35c599dd7fb0157946
ecampos <edwincampos@gmail.com>
2021-07-02 10:18:47 -0600

    se agrego para conectar a cloud.mongodb.com

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const sala = require('./routes/sala');
 const express = require('express');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+
+const USER = process.env.DB_USER;
+const PASSWORD= process.env.DB_PASS;
+const DATA_BASE= process.env.DB_DATANAME;
+
 //Conectar a la base de datos
-mongoose.connect('mongodb://localhost:27017/sala_reuniones', {useNewUrlParser: true, useUnifiedTopology: true})
+//mongoose.connect('mongodb://localhost:27017/sala_reuniones', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.ocdqj.mongodb.net/${DATA_BASE}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(()=> console.log('Connectado a MongoDB'))
-        .catch(()=> console.log('No se pudo conectar con MongoDB', err));
+        .catch(()=> console.log('No se pudo conectar con MongoDB'));
 
 const app = express();
 app.use(express.json());
@@ -21,7 +29,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/sala', sala);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.listen(port,() =>{
     console.log("Api para reserva de sala de reuniones.")
